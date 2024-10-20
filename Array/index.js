bbecd4737f4d06f454e6835a60ba81aa1d5bca90
Leviahegyrol9 <paplukacs.levi@gmail.com>
2024-10-20 16:46:06 +0200

    2024.10.20

--- Array/index.js
+++ Array/index.js
@@ -4,10 +4,10 @@ names.forEach((n) => {
     console.log(n);
 })
 
-const filter = names.filter((n) => n.length > 4)
+const filter = names.filter((n) => n.length > 4);
 
 console.log(filter);
 
-const search = names.find((n) => n.length > 4 && n.length < 10)
+const search = names.find((n) => n.length > 4 && n.length < 10);
 
 console.log(search);
