853de4be2e54f23b2ef83224054402459d65923a
Leviahegyrol9 <paplukacs.levi@gmail.com>
2024-11-24 14:53:38 +0100

    2024.11.24

--- Árcsökkentés/index.js
+++ Árcsökkentés/index.js
@@ -3,4 +3,4 @@ const percentage = 8.7;
 
 var result = cost - (cost * (percentage / 100));
 
-console.log(`Az alkatresz jelenlegi ara: ${cost}\nAremeles (szazalekban): ${percentage}\n\nAz alkatresz uj ara: ${result}`);
+console.log(`Az alkatresz jelenlegi ara: ${cost}\nÁrcsökkentés (szazalekban): ${percentage}\n\nAz alkatresz uj ara: ${result}`);
