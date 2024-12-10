42bf0228972f98b0d6108c8b34ae3b9ee9493169
Leviahegyrol9 <paplukacs.levi@gmail.com>
2024-12-10 19:45:00 +0100

    2024.12.10

--- Szorzótábla/index.js
+++
@@ -1,9 +0,0 @@
-var line = "";
-
-for(let i = 1; i <= 10; i++){
-    for(let j = 1; j <= 10; j++){
-        line += `${i * j}\t`;
-    }
-    console.log(line)
-    line = "";
-}
--- Árcsökkentés/index.js
+++
@@ -1,6 +0,0 @@
-const cost = 17500;
-const percentage = 8.7;
-
-var result = cost - (cost * (percentage / 100));
-
-console.log(`Az alkatresz jelenlegi ara: ${cost}\nÁrcsökkentés (szazalekban): ${percentage}\n\nAz alkatresz uj ara: ${result}`);
