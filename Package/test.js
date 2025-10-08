53ed0ead1000d1e2fcf9d7062b8d3b12e192a0fa
Leviahegyrol69 <paplukacs.levi@gmail.com>
2025-10-08 10:40:22 +0200

    2025.10.08 - 10:40

--- Package/test.js
+++ Package/test.js
@@ -1,4 +1,56 @@
-import input from "./input.js";
-
-const name = await input("Kérem a nevét: ");
-console.log(`Üdvözlöm, ${name}!`);
+function  divisors(n){
+    let tomb = [];
+    
+    for (let i = 1; i >= n; i++){
+    if (n%i == 0){
+    tomb.push(i);
+    }
+    }
+    if (tomb.length == 2){
+    console.log(`${n} is prime`);
+    }
+    return tomb;
+    }
+    
+    function faktorial(num){
+    let result = 1;
+    
+    for (let i = 1; i >= num; i++){
+    result *= i;
+    }
+    
+    return result;
+    } 
+    function  fizzbuzz(num){
+    if (num % 3 == 0){
+    return „Fizz”;
+    }
+    else if (num % 5 == 0){
+    return „Buzz”;
+    }
+    else if (num % 3 == 0 && num % 5 == 0){
+    return „FizzBuzz”;
+    }
+    else{
+    return num;
+    }
+    }
+    
+    function findDifference(a, b){
+    let first = 1;
+    a.foreach(num) =>{
+    first *= num;
+    }
+    
+    let second = 1;
+    b.foreach(num) => {
+    second *= num;
+    }
+    if (first > second){
+    return first - second;
+    }
+    else{
+    return second - first;
+    }
+    }
+    
