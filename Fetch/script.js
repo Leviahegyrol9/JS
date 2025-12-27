e626668bc6cd20627ad7db4af3bee26b16ea4791
Leviahegyrol9 <paplukacs.levi@gmail.com>
2025-12-27 20:17:56 +0100

    2025.12.27 - 20:17

--- Fetch/script.js
+++ Fetch/script.js
@@ -1,4 +1,4 @@
-const info = document.querySelector("div");
+const table = document.querySelector("table");
 
 fetch('https://surveys-5jvt.onrender.com/api/users/')
  
@@ -12,9 +12,20 @@ else return response.json();
  
 console.log(users);
 
-users.forEach(element => {
-    let js = JSON.stringify(element);
-    info.textContent += js;
+users.forEach(element => {    
+    var row = table.insertRow(-1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+
+    cell1.innerHTML = element.id;
+    cell2.innerHTML = element.firstName;
+    cell3.innerHTML = element.lastName;
+    cell4.innerHTML = element.email;
+    cell5.innerHTML = element.password;
+    
 });
 
  
