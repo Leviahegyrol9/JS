1fb9da7e2d42b21d7fa5b2b83b9b786a7974a5a2
Leviahegyrol9 <paplukacs.levi@gmail.com>
2026-09-23 20:29:19 +0200

    2026.09.23 - 20:29

--- old/Counter/script.js
+++
@@ -1,8 +0,0 @@
-const span = document.getElementById("counter");
-
-function Minus(num){
-    span.textContent = parseInt(span.textContent) - parseInt(num)
-}
-function Plus(num){
-    span.textContent = parseInt(span.textContent) + parseInt(num)
-}
